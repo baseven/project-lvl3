61977567675649758c87c217fd37208af1c8a9ef
Vladislav Akashev <vlad.akashev.w@gmail.com>
2021-02-08 18:05:28 +0300

    make babel set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log('Hello, world!');
+const text = 'Hello, world!';
+console.log(text);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },  
   plugins: [],
 };
