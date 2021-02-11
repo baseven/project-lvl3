ba1f9eae287ed8aeedc260a8c58ee151fcba94d7
Vladislav Akashev <vlad.akashev.w@gmail.com>
2021-02-11 22:44:24 +0300

    add and set up bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -17,6 +18,28 @@ module.exports = {
             presets: ['@babel/preset-env'],
           },
         },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          // inject CSS to page
+          { loader: 'style-loader' },
+          // translates CSS into CommonJS modules
+          { loader: 'css-loader' },
+          // Run postcss actions
+          {
+            loader: 'postcss-loader',
+            options: {
+            // `postcssOptions` is needed for postcss 8.x;
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: () => [autoprefixer],
+              },
+            },
+          },
+          // compiles Sass to CSS
+          { loader: 'sass-loader' },
+        ],
       },
     ],
   },
