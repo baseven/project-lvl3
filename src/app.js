1e10e3b684ec2c4c4fac1b841dfdb96c44c5db85
Vladislav Akashev <vlad.akashev.w@gmail.com>
2021-02-23 13:06:15 +0300

    add feeds downloand

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ import _ from 'lodash';
 import onChange from 'on-change';
 import axios from 'axios';
 import validate from './utils/validate';
-import renderError from './renderers/renderError';
+import renderFeedback from './renderers/renderFeedback';
+import renderFeeds from './renderers/renderFeeds';
+
 import parser from './utils/parser';
 
 const updateValidationState = (watchedState) => {
-  const errors = validate(watchedState);
-  watchedState.form.valid = _.isEqual(errors, {});
-  watchedState.form.errors = errors;
+  const error = validate(watchedState);
+  watchedState.form.valid = _.isEqual(error, null);
+  watchedState.form.error = error;
 };
 
 const errorMessages = {
@@ -21,25 +23,35 @@ export default () => {
   const state = {
     form: {
       processState: 'filling',
-      fields: {
-        url: '',
-      },
+      url: null,
       valid: true,
-      errors: {},
+      error: null,
     },
     links: [],
     feeds: [],
-    newsList: [],
+    posts: [],
   };
 
-  const form = document.getElementById('rss-form');
-  const input = form.elements.url;
+  const form = document.querySelector('.rss-form');
   const submitButton = form.querySelector('[type="submit"]');
 
-  const processStateHandler = (processState) => {
+  const processStateHandler = (processState, watchedState) => {
     switch (processState) {
       case 'filling': {
         submitButton.disabled = false;
+        break;
+      }
+      case 'sending': {
+        submitButton.disabled = true;
+        break;
+      }
+      case 'failed': {
+        renderFeedback(form, watchedState.form.error);
+        break;
+      }
+      case 'finished': {
+        renderFeedback(form, watchedState.form.error);
+        renderFeeds(watchedState.feeds);
         break;
       }
       default: {
@@ -51,15 +63,7 @@ export default () => {
   const watchedState = onChange(state, (path, value) => {
     switch (path) {
       case 'form.processState': {
-        processStateHandler(value);
-        break;
-      }
-      case 'form.valid': {
-        submitButton.disabled = !value;
-        break;
-      }
-      case 'form.errors': {
-        renderError(input, value);
+        processStateHandler(value, watchedState);
         break;
       }
       default: {
@@ -68,35 +72,37 @@ export default () => {
     }
   });
 
-  input.addEventListener('input', ({ target: { value } }) => {
-    watchedState.form.fields.url = value;
-    updateValidationState(watchedState);
-  });
-
   // http://feeds.bbci.co.uk/sport/football/rss.xml?edition=uk
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    watchedState.form.url = url;
+    updateValidationState(watchedState);
+    if (!watchedState.form.valid) {
+      watchedState.form.processState = 'failed';
+      watchedState.form.processState = 'filling';
+      return;
+    }
+
     watchedState.form.processState = 'sending';
     try {
-      const { url } = watchedState.form.fields;
       // const = `https://hexlet-allorigins.herokuapp.com/get?url=${url}`;
       const response = await axios.get(url);
-      watchedState.form.processState = 'finished';
       watchedState.links.push(url);
       const data = parser(response);
       const { title, description, posts } = data;
       watchedState.feeds.push({ title, description });
-      watchedState.newsList.push({ posts });
-      console.log(state);
+      watchedState.posts.push({ posts });
+      watchedState.form.processState = 'finished';
     } catch (err) {
-      // В реальных приложениях также требуется корректно обрабатывать сетевые ошибки
-      watchedState.form.processError = errorMessages.network.error;
+      watchedState.form.error = errorMessages.network.error;
       watchedState.form.processState = 'failed';
-      console.log(state);
-      // здесь это опущено в целях упрощения приложения
       throw err;
     }
-    // Обработка данных, например, отправка на сервер
-    // watchedState.registrationForm.data
+
+    watchedState.form.processState = 'filling';
+    form.reset();
+    form.elements.url.focus();
   });
 };
